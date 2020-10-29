574e6191b6cf841a405968a7f59f0d6808c21c32
Viktoriya Tkhoryk <tkhorykvi@mymail.vcu.edu>
2020-10-28 20:40:27 -0400

    API finished hw

---
+++ Instructions/starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "INSERT YOUR API KEY"
+
+# Google API Key
+g_key = "INSERT YOUR API KEY"
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-api-challenge
+
+All of my work for both WeatherPy and VacationPy are in the starter_code file.
